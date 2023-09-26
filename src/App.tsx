f38e61dab5714dd3c9aa2d832b913c2383756a84
ericchase <github@eric.addymail.com>
2023-09-26 13:30:46 -0500

    up
    Signed-off-by: ericchase <github@eric.addymail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import type { Component } from 'solid-js';
 
 const App: Component = () => {
-  return <p class="text-4xl text-green-700 text-center py-20">Hello tailwind!</p>;
+  return (
+    <>
+      <h1>This is a Header</h1>
+    </>
+  );
 };
 
 export default App;
